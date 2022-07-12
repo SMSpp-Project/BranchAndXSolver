2d6a9930b878ab5846cc177f6f6f2adff1361411
Federica Di Pasquale <Federica.DiPasquale@hotmail.com>
2022-07-12 15:55:02 +0200

    update

--- BranchAndXSolver.cpp
+++ BranchAndXSolver.cpp
@@ -23,22 +23,21 @@
 #include "BranchAndXSolver.h"
 
 /*--------------------------------------------------------------------------*/
-/*-------------------------------- MACROS ----------------------------------*/
+/*--------------------------------- MACROS ---------------------------------*/
 /*--------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------*/
-/*------------------------- NAMESPACE AND USING ----------------------------*/
+/*--------------------------- NAMESPACE AND USING --------------------------*/
 /*--------------------------------------------------------------------------*/
 
 using namespace SMSpp_di_unipi_it;
 
+/*--------------------------------------------------------------------------*/
+/*------------------------------- CONSTANTS --------------------------------*/
+/*--------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------*/
-/*-------------------------------- CONSTANTS -------------------------------*/
-/*--------------------------------------------------------------------------*/
-
-/*--------------------------------------------------------------------------*/
-/*-------------------------------- FUNCTIONS -------------------------------*/
+/*------------------------------- FUNCTIONS --------------------------------*/
 /*--------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------*/
@@ -53,7 +52,137 @@ SMSpp_insert_in_factory_cpp_0( BranchAndXSolver );
 /*--------------------- METHODS OF BranchAndXSolver ------------------------*/
 /*--------------------------------------------------------------------------*/
 
+void BranchAndXSolver::set_Block( Block * block ) {
 
+ if( block == f_Block )       // nothing to do        
+  return;
+ 
+ Solver::set_Block( block );  // attach to the new Block
+
+ f_RelaxationSolver->set_Block( f_Block );
+ f_sense = f_Block->get_objective_sense();
+ }
+
+/*--------------------------------------------------------------------------*/
+/*--------------------- METHODS FOR SOLVING THE MODEL ----------------------*/
+/*--------------------------------------------------------------------------*/
+/// 
+
+int BranchAndXSolver::compute( bool changedvars ) {
+ 
+ lock();                    // lock the mutex
+
+ process_outstanding_Modification();
+ 
+ /*-------------------------------------------------------------------------*/
+ 
+ // Queue of subproblems still to be processed
+ std::priority_queue< BaXnode * > Q;  
+
+ // The root node is the original problem
+ BaXnode * root = new BaXnode(); 
+
+ // Initialize the queue with the original problem   
+ Q.push( root );
+ 
+ // last processed node
+ BaXnode * old_node = nullptr;
+
+ // for each subproblem in the queue Q
+ for( ; ! Q.empty() ; Q.pop() ) {
+
+  // select new subproblem
+  auto node = Q.top();
+
+  // update current subproblem 
+  update_subproblem( old_node , node );
+  
+  // compute relaxation
+  f_RelaxationSolver->compute();
+
+  // update bounds of node
+  node->set_bound( f_RelaxationSolver->get_var_value() ); // lb o ub
+
+  // Pruning Rules- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+  double gap = + Inf< double >();  // to check if the problem has been solved
+
+  if( f_sense == Objective::eMin ) {       // if it is a minimization problem
+   f_ub = std::min( f_ub , RlxSlv->get_true_ub() );     // global upper bound
+   gap = std::abs( RlxSlv->get_true_ub() - node->bound() );        // gap
+   if( node->bound() >= f_ub ){
+    //std::cout << "pruned by bound\n";
+    continue; 
+    }
+   }
+  else
+  if( f_sense == Objective::eMax ) {       // if it is a maximization problem
+   f_lb = std::max( f_lb , RlxSlv->get_true_lb() );     // global lower bound 
+   gap = std::abs( RlxSlv->get_true_lb() - node->bound() );        // gap
+   if( node->bound() <= f_lb ){
+    //std::cout << "pruned by bound\n";
+    continue; 
+    }
+   }
+   
+   // check if the problem has been solved to optimality
+   if( gap < 1e-04 ) {
+    //std::cout << "pruned by feasibility\n";
+    continue;   
+    }
+
+
+  // Branch - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  std::vector< Change * > children = RlxSlv->branch();
+  for( auto child : children ) {
+   auto new_node = new BaXnode( node , node->depth() + 1 , child );  
+   Q.push( new_node );                  
+   node->add_child( new_node );
+   }
+
+  // Stopping conditions
+  // accuracy | max_iter | ...
+  }
+
+
+ /*-------------------------------------------------------------------------*/ 
+
+ f_obj = ( f_sense == Objective::eMax ) ? f_lb : f_ub;
+
+ Return_OK:
+ 
+ unlock();                  // unlock the mutex     
+ 
+ return( kOK );
+ 
+ } // end( BranchAndXSolver::compute() )
+
+/*--------------------------------------------------------------------------*/
+/*--------------------------- PRIVATE METHODS ------------------------------*/
+/*--------------------------------------------------------------------------*/
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+ /// update current subproblem and its path and reverse path from the root
+
+ void BranchAndXSolver::update_subproblem( BaXnode * old_node , 
+                                           BaXnode * node ) {
+
+  // if it is the root node
+  if( ! node->father() )
+   return;
+
+  // compute least common ancestor (lca)
+  std::vector< Change * > path;
+
+  // apply undoChg up to lca
+
+  // apply chg from lca to the current node - 1
+
+  // store last undoChg with node->set_undoChg()
+ 
+  old_node = node;
+  }
 /*--------------------------------------------------------------------------*/
 /*--------------------- End File BranchAndXSolver.cpp ----------------------*/
 /*--------------------------------------------------------------------------*/
--- BranchAndXSolver.h
+++ BranchAndXSolver.h
@@ -27,6 +27,13 @@
 /*------------------------------ INCLUDES ----------------------------------*/
 /*--------------------------------------------------------------------------*/
 
+#include <queue>
+
+#include "Block.h"
+#include "Change.h"
+#include "Objective.h"
+#include "RelaxationSolver.h"
+#include "Solution.h"
 #include "Solver.h"
 
 /*--------------------------------------------------------------------------*/
@@ -42,6 +49,68 @@ namespace SMSpp_di_unipi_it
 /*--------------------------------------------------------------------------*/
 /** @defgroup BranchAndXSolver_CLASSES Classes in BranchAndXSolver.h
  *  @{ */
+
+
+/*--------------------------------------------------------------------------*/
+/*--------------------------- CLASS BaXnode --------------------------------*/
+/*--------------------------------------------------------------------------*/
+/*--------------------------- GENERAL NOTES --------------------------------*/
+/*--------------------------------------------------------------------------*/
+/// 
+/**  */
+
+class BaXnode {
+ 
+ public:
+ 
+ using Index = Block::Index;
+
+  // - - - - - - - - - - Constructor and Destructor - - - - - - - - - - - - -
+ 
+  BaXnode( BaXnode * f = nullptr , Index depth = 0 , 
+           Change * c = nullptr ) : 
+           f_father( f ) , f_depth( depth ) , f_chg( c ) , 
+           f_undoChg( nullptr ) ,
+           f_bound( + Inf< double >() ) {}
+  
+  ~BaXnode() = default;
+ 
+ // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  void set_chg( Change * chg ) { f_chg = chg; }
+  
+  void set_undoChg( Change * undoChg ) {f_undoChg = undoChg;}
+  
+  void set_father( BaXnode * father ) { f_father = father; }
+  
+  void add_child( BaXnode * child ) { v_children.push_back( child ); }
+  
+  void set_bound( double bound ) { f_bound = bound; }
+
+  void set_depth( Index depth ) { f_depth = depth; }
+
+  Change * chg() { return f_chg; }
+  
+  Change * undoChg() { return f_undoChg; }
+  
+  BaXnode * father() { return f_father; }
+  
+  double bound() { return f_bound; }
+
+  Index depth() { return f_depth; }
+
+ // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+ protected:
+ 
+  Change * f_chg;      // change to apply w.r.t. father node
+  Change * f_undoChg;  // undo-change
+  BaXnode * f_father;                   // pointer to the father node
+  std::vector< BaXnode * > v_children;
+  double f_bound;
+  Index f_depth;
+
+}; // end( class BaXnode )
 
 /*--------------------------------------------------------------------------*/
 /*----------------------- CLASS BranchAndXSolver ---------------------------*/
@@ -67,6 +136,8 @@ public:
  *
  *  @{ */
 
+ using Index = Block::Index;
+ using Subset = Block::Subset;
 
 /** @} ---------------------------------------------------------------------*/
 /*--------------- CONSTRUCTING AND DESTRUCTING BranchAndXSolver ------------*/
@@ -76,7 +147,12 @@ public:
 
  /// constructor
 
- BranchAndXSolver() = default;
+ BranchAndXSolver() : f_RelaxationSolver( nullptr ) ,
+                      f_incumbent( nullptr ) , 
+                      f_lb( + Inf< double >() ) ,
+                      f_ub( - Inf< double >() ) ,
+                      f_obj( + Inf< double >() ) , 
+                      f_sense( Objective::eMin ) {}
 
 /*--------------------------------------------------------------------------*/
  /// destructor
@@ -90,10 +166,27 @@ public:
  *
  *  @{ */
 
+/*--------------------------------------------------------------------------*/
  /// set the (pointer to the) Block that the Solver has to solve
  /** */
 
- //void set_Block( Block * block ) override;
+ void set_Block( Block * block ) override;
+
+/*--------------------------------------------------------------------------*/
+ /// set the (pointer to the) the Relaxation Solver 
+ /** */
+
+ void set_RelaxationSolver( Solver * rlxslv ) {
+  
+  if( f_RelaxationSolver == rlxslv )
+   return;
+  
+  RlxSlv = dynamic_cast< RelaxationSolver * >( rlxslv );
+  if( ! RlxSlv )
+   throw( std::invalid_argument( "Not able to cast to RelaxationSolver *" ) );
+
+  f_RelaxationSolver = rlxslv;
+  }
 
 
 /** @} ---------------------------------------------------------------------*/
@@ -113,7 +206,7 @@ public:
 
  /// (try to) solve the Block
 
- int compute( bool changedvars = true ) override { return 0; }
+ int compute( bool changedvars = true ) override;
 
 /** @} ---------------------------------------------------------------------*/
 /*---------------------- METHODS FOR READING RESULTS -----------------------*/
@@ -121,7 +214,15 @@ public:
 /** @name Accessing the found solutions (if any) and solution information-
  *  @{ */
 
- void get_var_solution( Configuration * solc = nullptr ) override {}
+ void get_var_solution( Configuration * solc = nullptr ) override {
+  f_incumbent->write( f_Block );  
+ }
+
+/*--------------------------------------------------------------------------*/
+/// return the value of the (current) solution
+/** Return the the value of the current solution. */
+
+OFValue get_var_value() override { return( f_obj ); }
 
 /** @} ---------------------------------------------------------------------*/
 /*-------------- METHODS FOR READING THE DATA OF THE Solver ----------------*/
@@ -150,25 +251,44 @@ public:
 /*--------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------*/
-/*---------------------------- PROTECTED FIELDS  ---------------------------*/
+/*-------------------------- PROTECTED FIELDS  -----------------------------*/
 /*--------------------------------------------------------------------------*/
+ 
+ Solver * f_RelaxationSolver;          // pointer to the relaxation solver
+ RelaxationSolver * RlxSlv;
+ Solution * f_incumbent;               // incumbent solution 
 
- // algorthmic parameters - - - - - - - - - - - - - - - - - - - - - - - - - -
+ double f_lb;                          // global lower bound
+ double f_ub;                          // global upper bound
+ double f_obj;                         // value of the objective function 
+
+ int f_sense;                          // type of the optimization (min,max)
+ 
+ // algorithmic parameters - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 
 /*--------------------------------------------------------------------------*/
-/*--------------------- PRIVATE PART OF THE CLASS --------------------------*/
+/*----------------------- PRIVATE PART OF THE CLASS ------------------------*/
 /*--------------------------------------------------------------------------*/
 
  private:
 
 /*--------------------------------------------------------------------------*/
-/*-------------------------- PRIVATE METHODS -------------------------------*/
+/*--------------------------- PRIVATE METHODS ------------------------------*/
 /*--------------------------------------------------------------------------*/
 
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+ /// process all the pending modifications 
+
+ void process_outstanding_Modification() {}
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+ /// update current subproblem and its path and reverse path from the root
+
+ void update_subproblem( BaXnode * old_node , BaXnode * node );
  
 /*--------------------------------------------------------------------------*/
-/*------------------------------ PRIVATE FIELDS  ---------------------------*/
+/*---------------------------- PRIVATE FIELDS  -----------------------------*/
 /*--------------------------------------------------------------------------*/
 
 
